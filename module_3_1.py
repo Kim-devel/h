a5ab20d7ffda6d43c40fb17e1eca848be93c83ff
Kim-devel <kimsokolov81@gmail.com>
2024-11-18 20:34:58 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,28 @@
+# Глобальная переменная для подсчета вызовов
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1  # Увеличивает значение глобальной переменной calls.
+
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.lower() in (item.lower() for item in list_to_search)
+
+
+# Пример вызовов функций
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+
+# Вывод количества вызовов
+print(calls)
